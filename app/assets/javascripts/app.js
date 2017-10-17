6be97cdb99a100104fbd865dc4f67d28074e36bf
MrFuentastic <vfuentes8587@gmail.com>
2017-10-17 12:00:39 -0500

    initial commit

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      tasks: [
+        {text: "testsingtesteststestestesticles", priority: 2 },
+        {text: "some stuff", priority: 3},
+        {text: "Ch-ch-ch-changes!", priority: 1}
+      ],
+      newTaskText: "",
+      newTaskPriority: "",
+    },
+    mounted: function() {
+
+    },
+    methods: {
+      addTask: function() {
+        if (this.newTaskText !== "" && this.newTaskPriority !== "") {
+          let newTask = {
+            text: this.newTaskText,
+            priority: this.newTaskPriority,
+          }
+          this.tasks.push(newTask);
+          this.newTaskText = "";
+          this.newTaskPriority = "";
+        }
+      },
+      taskComplete: function(currentTask) {
+        var index = this.tasks.indexOf(currentTask);
+        this.tasks.splice(index, 1);
+      },
+      sortTasks: function() {
+        
+      }
+    },
+    computed: {
+
+    }
+  });
+});
